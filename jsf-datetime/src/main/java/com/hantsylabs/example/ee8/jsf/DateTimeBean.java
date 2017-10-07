57eb64edeedbffdcc637264e0706d0ff8b25714e
hantsy <hantsy@gmail.com>
2017-10-07 16:37:38 +0800

    added  jsf datetime sample.

---
+++ jsf-datetime/src/main/java/com/hantsylabs/example/ee8/jsf/DateTimeBean.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hantsylabs.example.ee8.jsf;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZonedDateTime;
+import java.util.logging.Logger;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author hantsy
+ */
+@ViewScoped
+@Named("dateTimeBean")
+public class DateTimeBean implements Serializable {
+
+    private static final Logger LOG = Logger.getLogger(DateTimeBean.class.getName());
+
+    private LocalDate localDate = LocalDate.now();
+    private LocalTime localTime = LocalTime.now();
+    private LocalDateTime localDateTime = LocalDateTime.now();
+    private OffsetTime offsetTime = OffsetTime.now();
+    private OffsetDateTime offsetDateTime = OffsetDateTime.now();
+    private ZonedDateTime zonedDateTime = ZonedDateTime.now();
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public LocalTime getLocalTime() {
+        return localTime;
+    }
+
+    public void setLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public OffsetTime getOffsetTime() {
+        return offsetTime;
+    }
+
+    public void setOffsetTime(OffsetTime offsetTime) {
+        this.offsetTime = offsetTime;
+    }
+
+    public OffsetDateTime getOffsetDateTime() {
+        return offsetDateTime;
+    }
+
+    public void setOffsetDateTime(OffsetDateTime offsetDateTime) {
+        this.offsetDateTime = offsetDateTime;
+    }
+
+    public ZonedDateTime getZonedDateTime() {
+        return zonedDateTime;
+    }
+
+    public void setZonedDateTime(ZonedDateTime zonedDateTime) {
+        this.zonedDateTime = zonedDateTime;
+    }
+
+    
+}
