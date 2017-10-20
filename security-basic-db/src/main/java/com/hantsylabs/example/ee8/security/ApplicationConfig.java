8b0c0127ebb5f063268b2da98c1fd3bb21b56a21
hantsy <hantsy@gmail.com>
2017-10-20 14:59:10 +0800

    added DatabaseIdentityStoreDefinition sample.

---
+++ security-basic-db/src/main/java/com/hantsylabs/example/ee8/security/ApplicationConfig.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hantsylabs.example.ee8.security;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+import javax.security.enterprise.identitystore.Pbkdf2PasswordHash;
+
+/**
+ *
+ * @author hantsy
+ */
+@DatabaseIdentityStoreDefinition(
+    dataSourceLookup = "${'java:global/MyDS'}",
+    callerQuery = "#{'select password from caller where name = ?'}",
+    groupsQuery = "select group_name from caller_groups where caller_name = ?",
+    hashAlgorithm = Pbkdf2PasswordHash.class,
+    priorityExpression = "#{100}",
+    hashAlgorithmParameters = {
+        "Pbkdf2PasswordHash.Iterations=3072",
+        "${applicationConfig.dyna}"
+    } // just for test / example
+)
+@ApplicationScoped
+@Named
+public class ApplicationConfig {
+
+    public String[] getDyna() {
+        return new String[]{"Pbkdf2PasswordHash.Algorithm=PBKDF2WithHmacSHA512", "Pbkdf2PasswordHash.SaltSizeBytes=64"};
+    }
+
+}
